a65c6979feaaa9abb1da12f45de8ca2d3f63e5f8
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-03-04 14:28:03 -0600

    Completed second exercise.

---
+++ js02/js02.js
@@ -0,0 +1,9 @@
+alert("Hello! My name is Alex.");
+
+console.log("I love huevos rancheros.");
+console.log("Unfortunately, I have zero cats.");
+console.log("I love to eat lots of food at Dario's.");
+console.log("Usually, I take 2 or 3 naps a week.");
+console.log("I'd like to be so well-rested that I take less.");
+
+alert("See ya!");
